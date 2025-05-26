886df58858cffeadb388bef51d465a2ea46ff38a
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-05-26 23:46:46 +0330

    edit some part of database

--- dependencies.py
+++ dependencies.py
@@ -1,9 +1,6 @@
-# app/dependencies.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# رشته اتصال به دیتابیس PostgreSQL را جایگزین کنید
 DATABASE_URL = "postgresql://postgres:root@localhost/leavepro"
 
 engine = create_engine(DATABASE_URL)
@@ -11,7 +8,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    """سازماندهی جلسه دیتابیس برای هر درخواست"""
     db = SessionLocal()
     try:
         yield db
